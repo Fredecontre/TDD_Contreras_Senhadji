2007c2061e24e6520388006a150d55bd83763d5a
Fredecontre <fredecontre@gmail.com>
2022-12-22 15:02:01 +0100

    update test verify_database

--- test_verify_database.py
+++ test_verify_database.py
@@ -1,15 +1,24 @@
 import unittest
-from verify_database import verify_database
+from verify_database import verify_database, add_link,add_user
 
 class TestUtilisateurs(unittest.TestCase):
 
-    def test_add_user(self):
+    def test_get_links(self):
+        #Exemples corrects
         self.assertEqual(verify_database(),True)
-        # self.assertTrue(verify_database())
-        # #A ce stade du dévéloppement, les utilisateurs/mdp invalides sont quand même ajoutés à
-        # # la base de données
-        # self.assertFalse(not(verify_database()))
-        # self.assertEqual(verify_database(),True)
+        add_link('frede','1M!tdePasse','unlink')
+        self.assertTrue(verify_database())
+        add_user('validuser','B@nM0tDePasse')
+        self.assertTrue(verify_database())
+        
+        #Ajout d'un lien invalide et vérification que la fonction returne False
+        add_link('user','@S@fePassword12','l!ien inv@lide')
+        self.assertFalse(verify_database())
+
+        #Ajout d'un utilisateur invalide et vérification que la fonction returne False
+        add_user('aa','m)dpasse_pasn@n')
+        self.assertEqual(verify_database(),False)
+    
 
 if __name__ == '__main__':
     unittest.main()
