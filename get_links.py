fd4ffaf2d0f72efc83cc84c82d1c35e270505f5d
Fredecontre <fredecontre@gmail.com>
2022-12-27 11:19:11 +0100

    fonction de recupération des liens d'un utilisateur complète

--- get_links.py
+++ get_links.py
@@ -1,3 +1,72 @@
+import sqlite3
+#Open database
+conn = sqlite3.connect('utilisateurs.db')
+conn.row_factory = sqlite3.Row
+c = conn.cursor()
+
+def user_is_valid(username,password):
+   
+   l, u, s, d = 0, 0, 0, 0
+   capitalalphabets="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+   smallalphabets="abcdefghijklmnopqrstuvwxyz"
+   specialchar='""!@#$%^&*()-+?_=,<>/""'
+   digits="0123456789"
+
+   if (len(username) >= 3):
+       for i in username:
+           # vérifier s'il y a des caractères spéciaux dans le username
+           if(i in specialchar):
+               return False 
+   else:
+      return False 
+
+   if (len(password) >= 8):
+       for i in password:
+    
+           # compter les lettres minuscules
+           if (i in smallalphabets):
+               l+=1           
+    
+           # compter les lettres majuscules
+           if (i in capitalalphabets):
+               u+=1           
+    
+           # compter les chiffres
+           if (i in digits):
+               d+=1           
+    
+           # compter les caractères spéciaux
+           if(i in specialchar):
+               s+=1       
+               
+   if (l>=1 and u>=1 and s>=1 and d>=1 and l+s+u+d==len(password)):
+      #Vérifier si l'utilisateur appartient à la base de données
+      for raw in c.execute('SELECT * FROM  Utilisateurs'):
+         if raw["Username"] == username and raw["Password"] == password:
+            print("Valid user")
+            return True
+   else:
+       print("Invalid user")
+       return False
+
 
 def get_links(username,password):
-   pass
+   #vérifier si l'utilisateur existe et si le couple username/password est valide
+   if not(user_is_valid(username,password)):
+      print("Invalid user")
+      return False
+
+   id_user = c.execute("SELECT id FROM Utilisateurs WHERE Username = (?) AND Password = (?)",[username,password]).fetchone()
+   links = c.execute("SELECT * FROM Liens WHERE Id_utilisateur = (?)",[id_user['id']]).fetchall()
+
+   res=[]
+   for i in range(len(links)):
+      res.append(links[i]['link'])
+   print(res)
+   return res
+
+
+conn.commit()
+
+
+
