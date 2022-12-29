f2b4f800c9f6258abbe109d07404e0b3c02df68b
Abderraouf-Senhadji <raoufsenhadji99@gmail.com>
2022-12-29 15:24:13 +0100

    circle_perim function implementation

--- circle_perim.py
+++ circle_perim.py
@@ -1,2 +1,10 @@
+import math
 def circle_perim(radius):
-	pass
+
+	if (type(radius) != float) and (type(radius) != int) :
+		raise TypeError('Input must be either int or float')
+	
+	if radius <= 0:
+		raise ValueError('Input must be positive')
+		
+	return 2*math.pi*radius
