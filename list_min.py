2d1ad7e0adaad4f8f3058fadbbb12facfd9fcdda
Abderraouf-Senhadji <raoufsenhadji99@gmail.com>
2022-12-29 14:29:45 +0100

    list_min function code

--- list_min.py
+++ list_min.py
@@ -1,2 +1,16 @@
 def list_min(int_list):
-	pass
+	n = len(int_list)
+	
+	if n <= 0:
+		raise ValueError('Empty List')
+
+	min = int_list[0]
+	for i in range(n):
+	
+		if type(int_list[i]) != int:
+			raise TypeError('List element is not an integer')
+			
+		if int_list[i] < min:
+			min = int_list[i]
+	
+	return min
