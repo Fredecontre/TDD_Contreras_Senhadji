2e7a955b65e38b3109163fcca72eb3c60de041ca
Abderraouf-Senhadji <raoufsenhadji99@gmail.com>
2022-12-29 14:51:07 +0100

    add test_typeerror

--- test_sqrt_func.py
+++ test_sqrt_func.py
@@ -11,8 +11,13 @@ class TestSqrtFunc(unittest.TestCase):
 	def test_sqrt_func(self):
 		self.assertEqual(sqrt_func(49), 7)
 		self.assertEqual(sqrt_func(1.44), 1.2)
+		self.assertEqual(sqrt_func(1), 1)
 		self.assertEqual(sqrt_func(0.25), 0.5)
 		self.assertEqual(sqrt_func(0), 0)
+		
+	def test_typeerror(self):
+		self.assertRaises(TypeError, sqrt_func, [4, 9])
+		self.assertRaises(TypeError, sqrt_func, 'A')
 		
 if __name__ == '__main__':
 	unittest.main()
